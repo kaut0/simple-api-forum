f835eb5c102cefeb45e3b852cce67ee8fca33728
kaut0 <zilullah792@gmail.com>
2022-02-09 21:01:29 +0800

    user resource

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use App\Models\User;
 
 class UserController extends Controller
 {
     public function show($username)
     {
-        return User::where('username', $username)->select('username', 'created_at')->first();
+        return new UserResource(
+            User::where('username', $username)->first());
     }
 }
